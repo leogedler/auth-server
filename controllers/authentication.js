aeb1c999646b303cb85978ce8086b4816b3e0fd1
leonardogedler <leogedler@hotmail.com>
2018-03-18 21:58:02 -0400

    Created new model and controller

---
+++ controllers/authentication.js
@@ -0,0 +1,5 @@
+const signup = (req, res, next) => {
+    res.send({ success: 'true' });
+}
+
+module.exports = { signup };
---
+++ router.js
@@ -0,0 +1,5 @@
+const { signup } = require('./controllers/authentication');
+
+module.exports = (app) => {
+    app.post('/signup', signup);
+}
